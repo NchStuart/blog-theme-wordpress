53274ce205ea361d79d9d631f0c2ee75c107f813
Nicholas Stuart <stuartwmv@gmail.com>
2023-09-27 18:26:23 -0300

    Inicialização do header, arquivos setup e adição do bootstrap/templatestyles

--- header.php
+++ header.php
@@ -1,9 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
   <?php wp_head(); ?>
-
 </head>
 
-<body>
+<body <?php body_class(); ?>>
+
+  <header>
+
+    <div class="top_header">
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="collapse navbar-collapse">
+            <?php 
+            if(has_nav_menu('top')) {
+              wp_nav_menu(array(
+                'theme_location' => 'top',
+                'container' => false,
+                'fallback_cb' => false,
+                'menu_class' => 'nav navbar-nav'
+              ));
+            } 
+            ?>
+          </div>
+        </div>
+      </nav>
+    </div>
+    <div class="main_header">
+      <div class="container"></div>
+    </div>
+
+  </header>
--- include/setup.php
+++ include/setup.php
@@ -1,10 +1,10 @@
 <?php
 
 function nb_theme_styles() {
-  wp_enqueue_style('bootstrap_css', get_template_directory_uri().'assets/css/bootstrap.min.css');
-  wp_enqueue_style('template_css', get_template_directory_uri().'assets/css/template.css');
-  wp_enqueue_script( 'bootstrap_js', get_template_directory_uri().'assets/js/bootstrap.min.js' );
-  wp_enqueue_script( 'script_js', get_template_directory_uri().'assets/js/script.js' );
+  wp_enqueue_style('bootstrap_css', get_template_directory_uri().'/assets/css/bootstrap.min.css');
+  wp_enqueue_style('template_css', get_template_directory_uri().'/assets/css/template.css');
+  wp_enqueue_script( 'bootstrap_js', get_template_directory_uri().'/assets/js/bootstrap.min.js' );
+  wp_enqueue_script( 'script_js', get_template_directory_uri().'/assets/js/script.js' );
 }
 
 function nb_after_setup() {
